2d01c4ace046b5b2f8f0c3b20b940ad0fd3f80b1
Pasamvamsi <122425402+Pasamvamsi@users.noreply.github.com>
2024-06-07 08:35:34 +0530

    Add files via upload

---
+++ c2(1).py
@@ -0,0 +1,26 @@
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Python code to Count Alphabets, Special character Numeric values and space 
+... string=input("Please enter a string: ")#take input from the user 
+... alphabets,num,special,space=0,0,0,0;#variable declaration and initilization
+... a=[]
+... d=[]
+... spl=[]
+... for i in range(len(string)): 
+...   if(string[i].isalpha()): #check Alphabets letters 
+...       #print(string[i],end="")
+...       alphabets+=1
+...       a.append(string[i])
+...   elif(string[i].isdigit()):#check numeric value 
+...       #print(string[i],end="")
+...       num+=1
+...       d.append(string[i])
+...   elif(string[i].isspace()):#check space 
+...       space+=1 
+...   else: 
+...       #print(string[i],end="")
+...       special+=1
+...       spl.append(string[i])
+... print("Alpabets letters: ",alphabets, a)
+... print("\nnumbers: ",num, d) 
+... print("\nSpace: ",space) 
